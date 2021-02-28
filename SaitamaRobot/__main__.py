90abfe96a4d7d98da683eedd7cd922c8967ac59c
shiwabot <71938018+shiwabot@users.noreply.github.com>
2021-02-28 11:28:57 +0530

    Update __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -77,7 +77,7 @@ And the following:
     dispatcher.bot.first_name, ""
     if not ALLOW_EXCL else "\nAll commands can either be used with / or !.\n")
 
-SAITAMA_IMG = "https://telegra.ph/file/664a13c95fae81ebb222a.jpg"
+SAITAMA_IMG = "https://telegra.ph/file/64a230c34720389939cd7.jpg"
 DONATE_STRING = """Anie doesn't need any donations as of now. Thank """ 
 
 
