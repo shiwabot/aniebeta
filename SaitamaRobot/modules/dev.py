3f389d27aec4313fad1a393246a63738202c3a39
DENVILBOT <71938018+shiwabot@users.noreply.github.com>
2021-07-07 13:01:50 +0530

    Add files via upload

---
+++ SaitamaRobot/modules/dev.py
@@ -0,0 +1,103 @@
+import os
+import subprocess
+import sys
+from time import sleep
+from telegram.error import BadRequest
+from telegram import ParseMode
+from telegram.utils.helpers import escape_markdown
+
+from SaitamaRobot import dispatcher
+from SaitamaRobot.modules.helper_funcs.chat_status import dev_plus, asse_plus
+from telegram import TelegramError, Update
+from telegram.ext import CallbackContext, CommandHandler, run_async
+from SaitamaRobot import DEV_USERS, SUDO_USERS
+from SaitamaRobot.modules.helper_funcs.filters import CustomFilters
+
+
+@run_async
+@asse_plus
+def leave(update: Update, context: CallbackContext):
+    bot = context.bot
+    args = context.args
+    if args:
+        chat_id = str(args[0])
+        try:
+            bot.leave_chat(int(chat_id))
+            update.effective_message.reply_text("Beep boop, I left that soup!.")
+        except TelegramError:
+            update.effective_message.reply_text(
+                "Beep boop, I could not leave that group(dunno why tho).")
+    else:
+        update.effective_message.reply_text("Send a valid chat ID")
+
+@run_async
+def slist(update, context):
+    message = update.effective_message
+    text1 = "*Sudo List*👑"
+    text2 = "*Developers*"
+    for user_id in SUDO_USERS:
+        try:
+            user = context.bot.get_chat(user_id)
+            name = "[{}](tg://user?id={})".format(user.first_name + (user.last_name or ""), user.id)
+            if user.username:
+                name = escape_markdown("@" + user.username)
+            text1 += "\n - {}".format(name)
+        except BadRequest as excp:
+            if excp.message == 'Chat not found':
+                text1 += "\n - ({}) - not found".format(user_id)
+    for user_id in DEV_USERS:
+        try:
+            user = context.bot.get_chat(user_id)
+            name = "[{}](tg://user?id={})".format(user.first_name + (user.last_name or ""), user.id)
+            if user.username:
+                name = escape_markdown("@" + user.username)
+            text2 += "\n - {}".format(name)
+        except BadRequest as excp:
+            if excp.message == 'Chat not found':
+                text2 += "\n - ({}) - not found".format(user_id)
+    message.reply_text(text1 + "\n" + text2 + "\n", parse_mode=ParseMode.MARKDOWN)
+    
+
+
+@run_async
+@asse_plus
+def gitpull(update: Update, context: CallbackContext):
+    sent_msg = update.effective_message.reply_text(
+        "Pulling all changes from remote and then attempting to restart.")
+    subprocess.Popen('git pull', stdout=subprocess.PIPE, shell=True)
+
+    sent_msg_text = sent_msg.text + "\n\nChanges pulled...I guess.. Restarting in "
+
+    for i in reversed(range(5)):
+        sent_msg.edit_text(sent_msg_text + str(i + 1))
+        sleep(1)
+
+    sent_msg.edit_text("Restarted.")
+
+    os.system('restart.bat')
+    os.execv('start.bat', sys.argv)
+
+
+@run_async
+@asse_plus
+def restart(update: Update, context: CallbackContext):
+    update.effective_message.reply_text(
+        "Starting a new instance and shutting down this one")
+
+    os.system('restart.bat')
+    os.execv('start.bat', sys.argv)
+
+
+LEAVE_HANDLER = CommandHandler("leave", leave)
+GITPULL_HANDLER = CommandHandler("gitpull", gitpull)
+RESTART_HANDLER = CommandHandler("reboot", restart)
+SLIST_HANDLER = CommandHandler("slist", slist,
+                           filters=CustomFilters.sudo_filter | CustomFilters.support_filter)
+
+dispatcher.add_handler(LEAVE_HANDLER)
+dispatcher.add_handler(GITPULL_HANDLER)
+dispatcher.add_handler(RESTART_HANDLER)
+dispatcher.add_handler(SLIST_HANDLER)
+
+__mod_name__ = "Dev"
+__handlers__ = [LEAVE_HANDLER, SLIST_HANDLER, GITPULL_HANDLER, RESTART_HANDLER]
