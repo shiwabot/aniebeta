7d7a048c4a790ce5c1e4a63d2bad1e2bf4ef0c72
DENVILBOT <71938018+shiwabot@users.noreply.github.com>
2021-07-17 10:48:29 +0530

    Update __init__.py

--- SaitamaRobot/modules/sql/__init__.py
+++ SaitamaRobot/modules/sql/__init__.py
@@ -2,7 +2,6 @@ from SaitamaRobot import DB_URI
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
-from SaitamaRobot.modules.sql import antiArabic_sql as sql
 
 
 def start() -> scoped_session:
