24ed459ab22c07ab63bf47afeddec1c74f5a2d88
DENVILBOT <71938018+shiwabot@users.noreply.github.com>
2021-07-20 23:07:17 +0530

    Add files via upload

---
+++ SaitamaRobot/modules/musicplayer.py
@@ -0,0 +1,70 @@
+
+__mod_name__ = "VC PLAYER"
+__help__ = """
+          <b>》** ANIE ** 《<b>
+            
+ SHASAMUSIC plays plays music in your group's voice chat as well as channel voice chats
+ 
+ Assistant name >> @Anievcplay
+ 
+ Setting up 
+
+1) Make bot admin (Group and in channel if use cplay)
+2) Start a voice chat
+3) Try /play [song name] for the first time by an admin
+*) If userbot joined enjoy music, If not add @Anievcplay to your group and retry
+
+ Commands 
+
+=>> Song Playing 🎧 
+
+ - /play: Play song using youtube music
+ - /play [yt url] : Play the given yt url
+ - /dplay: Play song via deezer
+ - /splay: Play song via jio saavn
+  
+=>> Playback ⏯
+
+ - /player: Open Settings menu of player
+ - /skip: Skips the current track
+ - /pause: Pause track
+ - /resume: Resumes the paused track
+ - /end: Stops media playback
+ - /current: Shows the current Playing track
+ - /playlist: Shows playlist
+
+=>> Channel Music Play 🛠
+
+For linked group admins only:
+
+ - /cplay [song name] - play song you requested
+ - /cdplay [song name] - play song you requested via deezer
+ - /csplay [song name] - play song you requested via jio saavn
+ - /cplaylist - Show now playing list
+ - /cccurrent - Show now playing
+ - /cplayer - open music player settings panel
+ - /cpause - pause song play
+ - /cresume - resume song play
+ - /cskip - play next song
+ - /cend - stop music play
+ - /userbotjoinchannel - invite assistant to your chat
+
+ channel is also can be used instead of c
+
+  If you donlt like to play in linked group:
+
+   1) Get your channel ID.
+   2) Rename your group to: Channel Music: your_channel_id
+   3) Add @Anierobot_bot as Channel admin with full perms
+   4) add helper to channel
+   5) Simply send commands in your group.
+
+=>> More tools 🧑‍🔧
+
+ - /admincache: Updates admin info of your group. Try if bot isn't recognize admin
+ - /userbotjoin: Invite @Anievcplay Userbot to your chat
+
+*Player cmd and all other cmds except /play, /current  and /playlist  are only for admins with manage group
+<b> PLEASE NOTE THIS SERVICE IS UNSTABLE AND CAN BE STOPPED ANYTIME </b>
+"""
+
