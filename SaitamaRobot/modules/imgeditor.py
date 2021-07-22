4b15639c36f3235cf57245c3d133abf156f809aa
DENVILBOT <71938018+shiwabot@users.noreply.github.com>
2021-07-22 15:23:00 +0530

    Update imgeditor.py

--- SaitamaRobot/modules/imgeditor.py
+++ SaitamaRobot/modules/imgeditor.py
@@ -10,7 +10,7 @@ from pyrogram.types import (
 )
 
 # By @TroJanzHEX
-from EmceeBot.ImageEditor.edit_1 import (  # pylint:disable=import-error
+from SaitamaRobot.ImageEditor.edit_1 import (  # pylint:disable=import-error
     black_white,
     box_blur,
     bright,
@@ -18,7 +18,7 @@ from EmceeBot.ImageEditor.edit_1 import (  # pylint:disable=import-error
     mix,
     normal_blur,
 )
-from EmceeBot.ImageEditor.edit_2 import (  # pylint:disable=import-error
+from SaitamaRobot.ImageEditor.edit_2 import (  # pylint:disable=import-error
     cartoon,
     circle_with_bg,
     circle_without_bg,
@@ -28,13 +28,13 @@ from EmceeBot.ImageEditor.edit_2 import (  # pylint:disable=import-error
     sepia_mode,
     sticker,
 )
-from EmceeBot.ImageEditor.edit_3 import (  # pylint:disable=import-error
+from SaitamaRobot.ImageEditor.edit_3 import (  # pylint:disable=import-error
     black_border,
     blue_border,
     green_border,
     red_border,
 )
-from EmceeBot.ImageEditor.edit_4 import (  # pylint:disable=import-error
+from SaitamaRobot.ImageEditor.edit_4 import (  # pylint:disable=import-error
     inverted,
     removebg_plain,
     removebg_sticker,
@@ -44,7 +44,7 @@ from EmceeBot.ImageEditor.edit_4 import (  # pylint:disable=import-error
     rotate_270,
     round_sticker,
 )
-from EmceeBot.ImageEditor.edit_5 import (  # pylint:disable=import-error
+from SaitamaRobot.ImageEditor.edit_5 import (  # pylint:disable=import-error
     normalglitch_1,
     normalglitch_2,
     normalglitch_3,
@@ -56,7 +56,7 @@ from EmceeBot.ImageEditor.edit_5 import (  # pylint:disable=import-error
     scanlineglitch_4,
     scanlineglitch_5,
 )
-from EmceeBot.utils.pyrogram import pbot as Client
+from SaitamaRobot.utils.pyrogram import pbot as Client
 
 lel = 00000000
 # pylint:disable=import-error
