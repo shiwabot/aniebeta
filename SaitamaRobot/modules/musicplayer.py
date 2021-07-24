c66d30dfa1d74aa6885cf2b89e7ba1f0cfaaf12d
DENVILBOT <71938018+shiwabot@users.noreply.github.com>
2021-07-24 23:46:57 +0530

    Update musicplayer.py

--- SaitamaRobot/modules/musicplayer.py
+++ SaitamaRobot/modules/musicplayer.py
@@ -3,9 +3,6 @@ __mod_name__ = "FEDERATION "
 __help__ = """
           <b>》** ANIE ** 《<b>
             
- ANIE FEDERATION 
-"""
-
 Ah, group management. It's all fun and games, until you start getting spammers in, and you need to ban them. Then you need to start banning more, and more, and it gets painful.
 But then you have multiple groups, and you don't want these spammers in any of your groups - how can you deal? Do you have to ban them manually, in all your groups?
 No more! With federations, you can make a ban in one chat overlap to all your other chats.
