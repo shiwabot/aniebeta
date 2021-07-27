5acae1de8a5fee1c5c11548daa1db5e1a8357a60
DENVILBOT <71938018+shiwabot@users.noreply.github.com>
2021-07-27 11:21:35 +0530

    Update _updater.py

--- SaitamaRobot/modules/_updater.py
+++ SaitamaRobot/modules/_updater.py
@@ -6,7 +6,6 @@ from SaitamaRobot.events import register
 from os import remove, execle, path, environ
 import asyncio
 import sys
-from SaitamaRobot import GitCommandError, InvalidGitRepositoryError, NoSuchPathError
 from SaitamaRobot import OWNER_ID, telethn, UPSTREAM_REPO_URL
 
 async def gen_chlog(repo, diff):
