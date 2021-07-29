a266155644a1c8e941c4faf9ad5039b0690325a1
DENVILBOT <71938018+shiwabot@users.noreply.github.com>
2021-07-29 15:43:56 +0530

    Update karma_mongo.py

--- SaitamaRobot/modules/mongo/karma_mongo.py
+++ SaitamaRobot/modules/mongo/karma_mongo.py
@@ -36,7 +36,7 @@ async def user_global_karma(user_id) -> int:
 
 
 async def get_karmas(chat_id: int) -> Dict[str, int]:
-    karma = await karmadb.find_one({"chat_id": chat_id})
+    karma =  karmadb.find_one({"chat_id": chat_id})
     if not karma:
         return {}
     return karma['karma']
@@ -69,7 +69,7 @@ async def karma_on(chat_id: int):
     is_karma = await is_karma_on(chat_id)
     if is_karma:
         return
-    return  karmaonoffdb.delete_one({"chat_id_toggle": chat_id})
+    return await karmaonoffdb.delete_one({"chat_id_toggle": chat_id})
 
 
 async def karma_off(chat_id: int):
