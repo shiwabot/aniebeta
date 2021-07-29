12290a0c8c00b11d81f4e64b1f4ae9e77a24ce07
DENVILBOT <71938018+shiwabot@users.noreply.github.com>
2021-07-29 10:35:01 +0530

    Update dbfunc.py

--- SaitamaRobot/utils/dbfunc.py
+++ SaitamaRobot/utils/dbfunc.py
@@ -4,6 +4,7 @@ from typing import Dict, List, Union
 
 coupledb = db.couple
 karmadb = db.karma
+nsfwdb = db.nsfw
 
 # Couple Chooser
 
@@ -107,3 +108,26 @@ async def alpha_to_int(user_id_alphabet: str) -> int:
         user_id += str(index)
     user_id = int(user_id)
     return user_id
+
+# antinsfw function
+
+
+async def is_nsfw_on(chat_id: int) -> bool:
+    chat = nsfwdb.find_one({"chat_id": chat_id})
+    if not chat:
+        return True
+    return False
+
+
+async def nsfw_on(chat_id: int):
+    is_nsfw = await is_nsfw_on(chat_id)
+    if is_nsfw:
+        return
+    return nsfwdb.delete_one({"chat_id": chat_id})
+
+
+async def nsfw_off(chat_id: int):
+    is_nsfw = await is_nsfw_on(chat_id)
+    if not is_nsfw:
+        return
+    return nsfwdb.insert_one({"chat_id": chat_id})
