dda26def646b64e6e5901d37e38e3aade663014d
DENVILBOT <71938018+shiwabot@users.noreply.github.com>
2021-07-30 19:09:28 +0530

    Update cleaner.py

--- SaitamaRobot/modules/cleaner.py
+++ SaitamaRobot/modules/cleaner.py
@@ -39,6 +39,7 @@ def clean_blue_text_must_click(update: Update, context: CallbackContext):
     message = update.effective_message
     k = chat.get_member(update.effective_user.id)
     if k.status == 'administrator' or k.status == 'creator':
+                    return 
     
         if sql.is_enabled(chat.id):
             fst_word = message.text.strip().split(None, 1)[0]
