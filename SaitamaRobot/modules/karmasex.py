4a046b17c9f1d0f2a39e45bf6099497b487200dd
HeLLxGodLike <ysonu4951@gmail.com>
2021-07-31 23:29:37 +0530

    Update karmasex.py

--- SaitamaRobot/modules/karmasex.py
+++ SaitamaRobot/modules/karmasex.py
@@ -1,5 +1,5 @@
 from SaitamaRobot import pbot
-from SaitamaRobot.utils.dbfunc import (update_karma, get_karma, get_karmas,
+from SaitamaRobot.utils.dbfunctions import (update_karma, get_karma, get_karmas,
                                    int_to_alpha, alpha_to_int)
 from SaitamaRobot.utils.filter_groups import karma_positive_group, karma_negative_group
 from pyrogram import filters
