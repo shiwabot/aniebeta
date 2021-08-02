6ae1d0e17f3d855a613c201040d407410603d944
DENVILBOT <71938018+shiwabot@users.noreply.github.com>
2021-08-02 15:43:46 +0530

    Update adminmisc.py

--- SaitamaRobot/modules/adminmisc.py
+++ SaitamaRobot/modules/adminmisc.py
@@ -33,7 +33,7 @@ from telethon.tl.types import (
 )
 
 from SaitamaRobot import OWNER_ID
-from SaitamaRobot import telethon as telethn 
+from SaitamaRobot import telethn
 
 # =================== CONSTANT ===================
 PP_TOO_SMOL = "**The image is too small**"
