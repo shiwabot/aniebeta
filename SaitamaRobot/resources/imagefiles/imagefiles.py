20e84eb59eb3d20bfcc272b9d2e98c2f4d770e2c
DENVILBOT <71938018+shiwabot@users.noreply.github.com>
2021-10-03 18:07:50 +0530

    Create imagefiles.py

---
+++ SaitamaRobot/resources/imagefiles/imagefiles.py
@@ -0,0 +1,6 @@
+# This File Contains all the Picture Links used all over the Bot
+
+#Images in __main__.py
+EMILIA_IMG = "https://telegra.ph/file/7944090b9aca51ef8f562.jpg"
+EMILIA_START_IMG = "https://telegra.ph/file/7944090b9aca51ef8f562.jpg"
+MILIA_HELP_IMG = "https://telegra.ph/file/7944090b9aca51ef8f562.jpg"
