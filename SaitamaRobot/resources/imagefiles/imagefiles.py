b3722b59d06a26b702643af5e938d46aa9aae6e5
DENVILBOT <71938018+shiwabot@users.noreply.github.com>
2021-10-03 18:27:10 +0530

    Delete SaitamaRobot/resources/imagefiles directory

--- SaitamaRobot/resources/imagefiles/imagefiles.py
+++
@@ -1,6 +0,0 @@
-# This File Contains all the Picture Links used all over the Bot
-
-#Images in __main__.py
-EMILIA_IMG = "https://telegra.ph/file/7944090b9aca51ef8f562.jpg"
-EMILIA_START_IMG = "https://telegra.ph/file/7944090b9aca51ef8f562.jpg"
-MILIA_HELP_IMG = "https://telegra.ph/file/7944090b9aca51ef8f562.jpg"
