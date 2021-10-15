6e5a4f650a771737f71da31914fb85f14b6e5ce6
DENVIL <84806367+XDENVIL@users.noreply.github.com>
2021-10-15 14:01:16 +0530

    Update approval.py

--- SaitamaRobot/modules/approval.py
+++ SaitamaRobot/modules/approval.py
@@ -249,28 +249,7 @@ def callback_button(update, context):
     for target_user in approve_list:
         REDIS.srem(f'approve_list_{chat_id}', target_user)
         query.message.edit_text(f"Successfully Unapproved all user in this Chat.")
-    
       
-
-
-
-        
-__mod_name__ = "Approval"    
-
-__help__ = """ 
-\
-Sometimes, you might trust a user not to send unwanted content.
-Maybe not enough to make them admin, but you might be ok with auto warns, blacklists, and antiflood not applying to them.
-That's what approvals are for - approve of trustworthy users to allow them to send 
-Admin commands:
-- /approval: Check a user's approval status in this chat.
-Admin commands:
-- /approve: Approve of a user. Locks, blacklists, and antiflood won't apply to them anymore.
-- /unapprove: Unapprove of a user. They will now be subject to locks, blacklists, and antiflood again.
-- /approved: List all approved users.
-- /unapproveall: Unapprove ALL users in a chat. This cannot be undone.
-\
-"""    
 
 APPROVED_HANDLER = DisableAbleCommandHandler("approved", approved, filters=Filters.group)
 UNAPPROVE_ALL_HANDLER = DisableAbleCommandHandler(["unapproveall", "disapproveall"], unapproveall, filters=Filters.group)
