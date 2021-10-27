14921720843229d8e4e4a64ede258be21ace79fb
DENVIL <84806367+XDENVIL@users.noreply.github.com>
2021-10-28 01:18:49 +0530

    Update Cc.py

--- SaitamaRobot/modules/Cc.py
+++ SaitamaRobot/modules/Cc.py
@@ -1,4 +1,7 @@
-__help__ = """ 
+__mod_name__ = "CC-Check"
+
+__help__ = """
+
 - /au <cc>: Stripe Auth given CC 
 - /pp <cc>: Paypal 1$ Guest Charge 
 - /ss <cc>: Speedy Stripe Auth 
@@ -6,7 +9,6 @@ __help__ = """
 - /bin <bin>: Gather's Info About the bin 
 - /gen <bin>: Generates CC with given bin 
 - /key <sk>: Checks if Stripe key is LiveMore Gates Soon...**Note:** Format of cc is ccnum|mm|yy|cvv""" 
-__mod_name__ = "CC-Checker"
 
-© 2021 GitHub, Inc.
+""""
 
