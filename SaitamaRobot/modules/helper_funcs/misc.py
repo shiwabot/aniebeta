31c1a89e2df0edbf90679b61c1f63b9b25c47021
DENVIL <84806367+XDENVIL@users.noreply.github.com>
2021-10-27 16:08:40 +0530

    Update misc.py

--- SaitamaRobot/modules/helper_funcs/misc.py
+++ SaitamaRobot/modules/helper_funcs/misc.py
@@ -68,12 +68,12 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     # can only have a certain amount of buttons side by side
     if len(pairs) > 8:
         pairs = pairs[modulo_page * 8:8 * (modulo_page + 1)] + [
-            (EqInlineKeyboardButton("👈🏻", callback_data="{}_prev({})".format(prefix, modulo_page)),
-                EqInlineKeyboardButton("🌹", callback_data="saitama_back"),
+            (EqInlineKeyboardButton("back", callback_data="{}_prev({})".format(prefix, modulo_page)),
+                EqInlineKeyboardButton("🌹", callback_data="help_back"),
              EqInlineKeyboardButton("👉🏻", callback_data="{}_next({})".format(prefix, modulo_page)))]
 
     else:
-        pairs += [[EqInlineKeyboardButton("Home", callback_data="bot_start")]]
+        pairs += [[EqInlineKeyboardButton("Home", callback_data="help_start")]]
 
 
 
