5176f22cc1caba8ece3c7c250c82359a214d7a31
DENVIL <84806367+XDENVIL@users.noreply.github.com>
2021-10-28 00:59:28 +0530

    Update and rename misc(4).py to misc.py

--- SaitamaRobot/modules/helper_funcs/misc.py
+++ SaitamaRobot/modules/helper_funcs/misc.py
@@ -1,7 +1,7 @@
 from math import ceil
 from typing import Dict, List
 
-from LaylaRobot import NO_LOAD
+from SaitamaRobot import NO_LOAD
 from telegram import MAX_MESSAGE_LENGTH, Bot, InlineKeyboardButton, ParseMode
 from telegram.error import TelegramError
 
