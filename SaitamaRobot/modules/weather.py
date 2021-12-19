cf23b9cd41624e74fa2fca7ef407fd4f04d751fd
DENVILBOT <71938018+shiwabot@users.noreply.github.com>
2021-12-19 18:15:00 +0530

    Update weather.py

--- SaitamaRobot/modules/weather.py
+++ SaitamaRobot/modules/weather.py
@@ -8,10 +8,10 @@ from telethon.tl import functions
 from telethon.tl import types
 from telethon.tl.types import *
 
-from Yuriko import *
+from SaitamaRobot import *
 
-from Yuriko import telethn as tbot
-from Yuriko.events import register
+from SaitamaRobot import telethn as tbot
+from SaitamaRobot.events import register
 
 
 async def is_register_admin(chat, user):
