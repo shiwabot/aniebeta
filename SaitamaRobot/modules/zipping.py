7114f295814ba97a6546ff03ae01171ab8d8f42a
DENVILBOT <71938018+shiwabot@users.noreply.github.com>
2021-12-19 18:13:10 +0530

    Update zipping.py

--- SaitamaRobot/modules/zipping.py
+++ SaitamaRobot/modules/zipping.py
@@ -5,9 +5,9 @@ import zipfile
 from telethon import types
 from telethon.tl import functions
 
-from Yuriko import TEMP_DOWNLOAD_DIRECTORY
-from Yuriko import telethn as client
-from Yuriko.events import register
+from SaitamaRobot import TEMP_DOWNLOAD_DIRECTORY
+from SaitamaRobot import telethn as client
+from SaitamaRobot.events import register
 
 
 async def is_register_admin(chat, user):
