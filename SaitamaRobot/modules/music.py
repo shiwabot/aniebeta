e69f507e80c436deacfa794daeef57cac0648fb9
DENVILBOT <71938018+shiwabot@users.noreply.github.com>
2021-12-19 15:23:02 +0530

    Update music.py

--- SaitamaRobot/modules/music.py
+++ SaitamaRobot/modules/music.py
@@ -14,8 +14,8 @@ from tswift import Song
 from yt_dlp import YoutubeDL
 from youtube_search import YoutubeSearch
 from youtubesearchpython import SearchVideos
-from Yuriko.utils.pluginhelper import get_text, progress
-from Yuriko import pbot, arq
+from SaitamaRobot.utils.pluginhelper import get_text, progress
+from SaitamaRobot import pbot, arq
 
 async def lyrics_func(answers, text):
     song = await arq.lyrics(text)
